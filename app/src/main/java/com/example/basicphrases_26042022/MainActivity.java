39a8bd319e7d98b860b7b988f6466ef841527e4c
fgromadan <gfromadan15@gmail.com>
2022-04-27 22:49:46 +0700

    Basic Phrases

---
+++ app/src/main/java/com/example/basicphrases_26042022/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.basicphrases_26042022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view){
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.robpercival.basicphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("buttton tapped", ourId);
+
+    }
+
+}
